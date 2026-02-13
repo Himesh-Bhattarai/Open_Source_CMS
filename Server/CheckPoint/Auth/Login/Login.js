a405b139663c8a4dcdd8b83dc686d90373c50c1a
Himeshchanchal Bhattarai <144266989+Himesh-Bhattarai@users.noreply.github.com>
2026-02-13 20:50:31 +0545

    status(301) : In projection need cookies to TRUE for testing in server there is true [IMP]

--- Server/CheckPoint/Auth/Login/Login.js
+++ Server/CheckPoint/Auth/Login/Login.js
@@ -72,14 +72,14 @@ export const loginCheckpoint = async (req, res, next) => {
     //send token in cookies both access and refresh
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: true,
+      secure: false, // Set to true in production with HTTPS
       sameSite: "lax",
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: true,
+      secure: false, // Set to true in production with HTTPS
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
